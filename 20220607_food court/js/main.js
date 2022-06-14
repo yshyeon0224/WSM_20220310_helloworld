61790f22611cf2550a6e795d87e9c099d79b5fa1
성현 <w2122@e-mirim.hs.kr>
2022-06-14 14:59:30 +0900

    js 여러가지 함수 복습

---
+++ 20220607_food court/js/main.js
@@ -0,0 +1,10 @@
+// function say(){
+//     console.log('hello wolrd');
+// }
+
+// const say = function () {
+//     console.log('hello world2');
+// }
+
+const say = () => console.log('hello world3');
+say();
